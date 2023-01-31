a34f349fc5ed40a0921f1505476b22344b405577
Your Name <nanawarechaitali4196@gmail.com>
2023-01-31 14:58:37 +0530

    Added Calculating Daily Wage

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -2,13 +2,20 @@ public class EmployeeWageComputation {
     public static void main(String[] args) {
         int Is_Present =1;
         int Is_Absent=0;
+        int Full_Time=1;
+        int Wage_Per_Hr =20;
         int checkAttendence =(int)(Math.random()*100)%2;
-        if(checkAttendence==Is_Present){
+        int workingHours=0;
+
+        if(checkAttendence==Full_Time){
             System.out.println("Employee is present");
+            workingHours=8;
 
         }else{
             System.out.println("Employee is absent");
         }
+        int wage = workingHours*Wage_Per_Hr;
+        System.out.println("Employee Daily Wage is" + wage);
 
     }
 }
