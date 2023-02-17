847c37f31f245b59f42f9e681132d05a4a96a10a
Your Name <nanawarechaitali4196@gmail.com>
2023-02-17 15:46:48 +0530

    RefactorCode

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -13,39 +13,42 @@ public class EmployeeWageComputation {
     private static final int Total_Working_Day_Per_Month=20;
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage computation program on Master branch");
+        calculateEmpWage();
+    }
+    public static void calculateEmpWage() {
 
         Random random = new Random();
         int attendance;
-        int total_working_days=0;
-        int total_emp_hours=0;
-        int total_emp_wage=0;
-        while(total_emp_hours<=100 && total_working_days<20){
-            attendance =random.nextInt(3);
-            switch(attendance){
+        int total_working_days = 0;
+        int total_emp_hours = 0;
+        int total_emp_wage = 0;
+        while (total_emp_hours <= 100 && total_working_days < 20) {
+            attendance = random.nextInt(3);
+            switch (attendance) {
                 case Is_Full_Time:
-                    total_emp_hours+=Full_Day_Hr;
+                    total_emp_hours += Full_Day_Hr;
                     total_working_days++;
                     break;
 
                 case Is_Part_Time:
-                    total_emp_hours+=Half_Day_Hr;
+                    total_emp_hours += Half_Day_Hr;
                     total_working_days++;
                     break;
                 case Is_Absent_Today:
-                    total_emp_wage+=Absent_Day_Hr;
+                    total_emp_wage += Absent_Day_Hr;
                     total_working_days++;
                     break;
 
                 default:
-                    total_emp_hours=Absent_Day_Hr;
+                    total_emp_hours = Absent_Day_Hr;
 
             }
         }
 
-      total_emp_wage+=Wage_Per_Hr*total_emp_hours;
-        System.out.println("Total working days"+total_working_days);
-        System.out.println("total working hours"+total_emp_hours);
-        System.out.println("Total wage for a month" +total_emp_wage);
+        total_emp_wage += Wage_Per_Hr * total_emp_hours;
+        System.out.println("Total working days" + total_working_days);
+        System.out.println("total working hours" + total_emp_hours);
+        System.out.println("Total wage for a month" + total_emp_wage);
 
 
     }
